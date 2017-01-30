4ce54e8b49fbfcbdb747d60c1dfb8995c9fc8803
Jasper Moeys <jaspermoeys@hotmail.com>
2017-01-30 18:41:43 +0100

    add support for dependent types to typeclass summoner

--- core/src/main/scala/simulacrum/typeclass.scala
+++ core/src/main/scala/simulacrum/typeclass.scala
@@ -332,7 +332,9 @@ class TypeClassMacros(val c: Context) {
 
     def generateCompanion(typeClass: ClassDef, tparam0: TypeDef, proper: Boolean, comp: Tree) = {
       val tparam = eliminateVariance(tparam0)
-      val summoner = q"@scala.inline def apply[$tparam](implicit instance: ${typeClass.name}[${tparam.name}]): ${typeClass.name}[${tparam.name}] = instance"
+      val instance = TermName("instance")
+      val refinedType = refinedInstanceTypeTree(typeClass, tparam, instance)
+      val summoner = q"@scala.inline def apply[$tparam](implicit $instance: ${typeClass.name}[${tparam.name}]): $refinedType = instance"
 
       val liftedTypeArgs = if (proper) List.empty[TypeDef] else {
         // We have a TypeClass[F[_ >: L <: U]], so let's create a F[X >: L <: U] for a fresh name X
